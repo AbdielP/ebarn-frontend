fc2fe152cd320dcb1b5cf43604d4ab96a0afd9ca
AbdielP <abdiel.pinzonc@gmail.com>
2020-12-23 21:02:08 -0500

    Se inicializa proyecto de ofertas electronicas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
+import { HomeModule } from './public/pages/home/home.module';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home', loadChildren: () => import('./public/pages/home/home.module').then(m => m.HomeModule)
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {
+      useHash: true, scrollPositionRestoration: 'enabled'
+    })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
