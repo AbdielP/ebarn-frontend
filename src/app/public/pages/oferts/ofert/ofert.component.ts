e581f6e70ef562536d2c8ba12f0f0b89660908fe
AbdielP <abdiel.pinzonc@gmail.com>
2020-12-27 21:16:08 -0500

    Se obtiene código de producto por params. pendiente iniciar servicios para ofertas

--- src/app/public/pages/oferts/ofert/ofert.component.ts
+++ src/app/public/pages/oferts/ofert/ofert.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-ofert',
   templateUrl: './ofert.component.html',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfertComponent implements OnInit {
 
-  constructor() { }
+  params: any;
+
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getParams();
+  }
+
+  getParams(): void {
+    this.activatedRoute.params.subscribe(params => {
+      this.params = params.codpr;
+      console.log(this.params);
+      // TENGO EL CODIGO, LLAMAR LOS SERVICIOS DE LAS OFERTAS..
+    });
   }
 
 }
