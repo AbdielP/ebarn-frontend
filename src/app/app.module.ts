69acccc7b34f3ab67f326521c02068c31b4cfb8e
AbdielP <abdiel.pinzonc@gmail.com>
2021-01-05 05:21:33 -0500

    Se implementa template y rutas de login, pendiente centrar html con css en angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from './forms/pages/forms.module';
 import { PublicModule } from './public/pages/public.module';
 import { AdminModule } from './admin/pages/admin.module';
 import { BrowserModule } from '@angular/platform-browser';
@@ -5,7 +6,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
--- src/app/forms/pages/forms-routing.module.ts
+++ src/app/forms/pages/forms-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsComponent } from './forms.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'forms', component: FormsComponent, children: [
+    { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule)}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
