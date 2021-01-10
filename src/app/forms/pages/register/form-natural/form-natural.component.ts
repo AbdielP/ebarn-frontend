89cfeb67d46ecd2e36f309caf5f99c3aa3f5e47c
AbdielP <abdiel.pinzonc@gmail.com>
2021-01-09 20:24:33 -0500

    Versión final de formulario register pn con validación por código. falta implementar formarray

--- src/app/forms/pages/register/form-natural/form-natural.component.ts
+++ src/app/forms/pages/register/form-natural/form-natural.component.ts
@@ -1,3 +1,4 @@
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormNaturalComponent implements OnInit {
 
-  constructor() { }
+  registerForm: any;
+  phones: any;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      name: ['', {validators: [Validators.required]}],
+      lastname: ['', {validators: [Validators.required]}],
+      idnumber: ['', {validators: [Validators.required]}],
+      province: ['', {validators: [Validators.required]}],
+      city: ['', {validators: [Validators.required]}],
+      address: ['', {validators: [Validators.required]}],
+      phone: [''],
+      cellphone: [''],
+      extratelephones: this.formBuilder.array([]),
+      email: ['', {validators: [Validators.required]}],
+      password: ['', {validators: [Validators.required, Validators.minLength(8)]}]
+    });
+  }
+
+  get telephones(): FormArray {
+    this.phones = this.registerForm.get('extratelephones') as FormArray;
+    return this.registerForm.get('extratelephones') as FormArray;
+  }
+
+  onSubmit(): void {
+    console.log(this.registerForm.getRawValue());
+  }
+
+  addExtraPhones(): void {
+    const extraphones = this.formBuilder.group({
+      phone: [''],
+      cell: ['']
+    });
+    this.telephones.push(extraphones);
   }
 
 }
--- src/app/forms/pages/register/register.module.ts
+++ src/app/forms/pages/register/register.module.ts
@@ -6,12 +6,15 @@ import { RegisterComponent } from './register.component';
 import { AccountTypeComponent } from './account-type/account-type.component';
 import { FormNaturalComponent } from './form-natural/form-natural.component';
 import { FormJuridicaComponent } from './form-juridica/form-juridica.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [RegisterComponent, AccountTypeComponent, FormNaturalComponent, FormJuridicaComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     RegisterRoutingModule
   ]
 })
