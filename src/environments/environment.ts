7a8d7b2b233087e5988591da35eafa7faefbcc75
AbdielP <abdiel.pinzonc@gmail.com>
2021-01-31 03:10:17 -0500

    se cambia servidor a heroku en variables de entorno

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  SERVER_URL: 'http://localhost:3000'
+  // SERVER_URL: 'http://localhost:3000'
+  SERVER_URL: 'https://ebarn-backend.herokuapp.com'
 };
 
 /*
